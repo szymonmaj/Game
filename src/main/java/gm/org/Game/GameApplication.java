24c9ed35c9b160a08a7cdb4af8b84c0eb31f4ea3
Szymon Maj <jasm0012@interia.pl>
2022-03-13 00:13:10 +0100

    Remove jframe

--- src/main/java/gm/org/Game/GameApplication.java
+++ src/main/java/gm/org/Game/GameApplication.java
@@ -2,16 +2,14 @@ package gm.org.Game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import javax.swing.*;
+
+
 
 @SpringBootApplication
 public class GameApplication {
 
-	public static void main(String[] args) {
-	JFrame frame1 = new JFrame ("Puste okno");
-	frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	frame1.setBounds(100, 100, 200, 200);
-	frame1.setVisible(true);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GameApplication.class, args);
+    }
 
 }
