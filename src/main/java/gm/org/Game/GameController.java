c10b1c62b41ad6b87c772852cbe23230028406e3
Szymon Maj <jasm0012@interia.pl>
2022-03-14 00:28:29 +0100

    Add calc for adding method

--- src/main/java/gm/org/Game/GameController.java
+++ src/main/java/gm/org/Game/GameController.java
@@ -4,12 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Scanner;
+
 @Controller
 public class GameController {
 
     @GetMapping("/")
     @ResponseBody
-    public String hello() {
-        return "Hello World!";
+    public static void hello() {
+        Scanner reader = new Scanner(System.in);
+        while(true) {
+            System.out.println("Wpisz liczbę:");
+            try {
+                int input1 = reader.nextInt();
+                System.out.println("Wpisz kolejną liczbę:");
+                int input2 = reader.nextInt();
+                int sum = input1 + input2;
+                System.out.println("Wynik: " + sum);
+            } catch (NumberFormatException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
+
 }
